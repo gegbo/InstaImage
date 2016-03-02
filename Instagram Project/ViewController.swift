25140e8f8042f8c782ddc0d9e190547329dd40ed
gegbo <graceegbo@graces-mac.local>
2016-03-01 18:13:28 -0600

    Initial Commit

---
+++ Instagram Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram Project
+//
+//  Created by Grace Egbo on 3/1/16.
+//  Copyright © 2016 Grace Egbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
